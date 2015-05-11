078f3c5ca48ae26de7be6c9246acdbe3cabc19c0
André Ornélio Fagundes <andre.ofag@hotmail.com>
2015-05-11 03:56:52 -0400

    hash exercise

---
+++ Exercise03.rb
@@ -0,0 +1,13 @@
+movies = {
+	:o_vento_levou => 1975,
+	:click => 2004,
+	:the_millers => 2013,
+	:sweet_november => 2001,
+	:et => 1981
+}
+
+puts movies[:o_vento_levou]
+puts movies[:click]
+puts movies[:the_millers]
+puts movies[:sweet_november]
+puts movies[:et]
